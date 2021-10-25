e568b840885b81f79a3e31f2ce40b071fe6f0791
jlholt2 <loganholt777@gmail.com>
2021-10-25 13:07:32 -0400

    fruit update

--- JumpyStreet/Assets/Scripts/Fruit.cs
+++ JumpyStreet/Assets/Scripts/Fruit.cs
@@ -11,20 +11,35 @@ public class Fruit : MonoBehaviour
 
     public void DetermineFruitType()
     {
-        switch (Random.Range(0, 3))
+        int randVal = Random.Range(0, 7);
+        if(randVal >= 6)
         {
-            case 1:
-                fruitSR.sprite = fruitSprites[1];
-                fruitType = FruitType.Cherry;
-                break;
-            case 2:
-                fruitSR.sprite = fruitSprites[2];
-                fruitType = FruitType.Orange;
-                break;
-            default:
-                fruitSR.sprite = fruitSprites[0];
-                fruitType = FruitType.Banana;
-                break;
+            fruitSR.sprite = fruitSprites[2];
+            fruitType = FruitType.Orange;
+        }else if(randVal >= 4)
+        {
+            fruitSR.sprite = fruitSprites[1];
+            fruitType = FruitType.Cherry;
         }
+        else
+        {
+            fruitSR.sprite = fruitSprites[0];
+            fruitType = FruitType.Banana;
+        }
+        //switch (Random.Range(0, 3))
+        //{
+        //    case 1:
+        //        fruitSR.sprite = fruitSprites[1];
+        //        fruitType = FruitType.Cherry;
+        //        break;
+        //    case 2:
+        //        fruitSR.sprite = fruitSprites[2];
+        //        fruitType = FruitType.Orange;
+        //        break;
+        //    default:
+        //        fruitSR.sprite = fruitSprites[0];
+        //        fruitType = FruitType.Banana;
+        //        break;
+        //}
     }
 }
